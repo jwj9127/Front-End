b2787eeebac89903a5cc7e1c9d571cda2f69a682
jwj9127 <jwj9127@naver.com>
2024-11-13 22:20:28 +0900

    [feat]: 메인 레이아웃 추가

---
+++ src/app/client-root-layout.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from 'react';
+import '../../styles/globals.css';
+import { Provider } from 'react-redux';
+import store from '../../store/store';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <Provider store={store}>
+            <html lang="en">
+                <body>{children}</body>
+            </html>
+        </Provider>
+    );
+}
