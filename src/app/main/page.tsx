37f6b12d0848bd9df6cbcb9bbfcd893ea9cc8da8
jwj9127 <jwj9127@naver.com>
2024-11-26 15:14:26 +0900

    [feat]: 메인화면 UI 진행중 ( 현재 ASMR 완성 )

---
+++ src/app/main/page.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+import styles from '../../../styles/main/main.module.css';
+import ASMR from './_components/ASMR';
+import Background from './_components/Background';
+import Calendar from './_components/Calendar';
+import TodoList from './_components/TodoList';
+import { backgroundGetCurrentAPI } from '../../../store/main/backgrounAPI';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../../../store/store';
+
+interface Asmr {
+    id: string;
+    fileName: string;
+    url: string;
+    contentType: string;
+}
+
+type ModalType = "asmr" | "background" | "calendar" | "todo" | null;
+
+export default function Main() {
+
+    const [openModal, setOpenModal] = useState<ModalType>(null);
+
+    const dispatch = useDispatch<AppDispatch>();
+    // const userId = window.localStorage.getItem('userId');
+    const [backgroundImage, setBackgroundImage] = useState(null);
+    const [audioElement, setAudioElement] = useState<HTMLAudioElement | null>(null);
+    const [currentAsmr, setCurrentAsmr] = useState<Asmr | null>(null);
+
+    useEffect(() => {
+        // dispatch(backgroundGetCurrentAPI(userId!))
+        //     .unwrap()
+        //     .then((response) => {
+        //         console.log(response);
+        //         setBackgroundImage(response.url);
+        //     })
+        //     .catch((error) => {
+        //         console.log(error);
+        //     })
+    })
+
+    const openModalHandler = (modalType: ModalType) => {
+        if (openModal === modalType) {
+            setOpenModal(null);
+        } else setOpenModal(modalType);
+    };
+
+    const closeModalHandler = () => {
+        setOpenModal(null);
+        console.log('꺼짐')
+    };
+
+    const playAudio = (src: string, asmr: Asmr | null): void => {
+        if (audioElement) {
+            audioElement.pause(); // 현재 오디오를 정지
+        }
+
+        const newAudio = new Audio(src);
+        newAudio.loop = true;
+        newAudio.play();
+        setAudioElement(newAudio);
+        setCurrentAsmr(asmr);
+
+        newAudio.onended = () => {
+            setAudioElement(null); // 오디오가 끝났을 때 audioElement 초기화
+            setCurrentAsmr(null);
+        };
+    };
+
+    const stopAudio = () => {
+        if (audioElement) {
+            audioElement.pause(); // 현재 오디오를 정지
+            setAudioElement(null); // 오디오 요소 초기화
+            setCurrentAsmr(null);
+        }
+    };
+
+    return (
+        <>
+            <div
+                className={styles.backgroundImage}
+            // style={{ backgroundImage: `url(${backgroundImage})` }}
+            ></div >
+            <div className={styles.mypage}></div>,
+            <div className={styles.nav}>
+                <div
+                    className={styles.asmr}
+                    onClick={() => openModalHandler("asmr")}
+                >
+                    <ASMR
+                        playAudio={playAudio}
+                        stopAudio={stopAudio}
+                        currentAsmr={currentAsmr}
+                        isModalOpen={openModal === "asmr"}
+                        closeModal={closeModalHandler}
+                    />
+                </div>
+                <div
+                    className={styles.background}
+                    onClick={() => openModalHandler("background")}
+                >
+                    <Background
+                        isModalOpen={openModal === "background"}
+                        closeModal={closeModalHandler}
+                    />
+                </div>
+                <div
+                    className={styles.calendar}
+                    onClick={() => openModalHandler("calendar")}
+                >
+                    <Calendar
+                        isModalOpen={openModal === "calendar"}
+                        closeModal={closeModalHandler}
+                    />
+                </div>
+                <div
+                    className={styles.todolist}
+                    onClick={() => openModalHandler("todo")}
+                >
+                    <TodoList
+                        isModalOpen={openModal === "todo"}
+                        closeModal={closeModalHandler}
+                    />
+                </div>
+            </div>
+        </>
+    )
+}
