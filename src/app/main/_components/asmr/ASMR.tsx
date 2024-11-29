28763d94717bf594c8ba59dfdcbef38df08de722
jwj9127 <jwj9127@naver.com>
2024-11-29 15:41:50 +0900

    [docs]: 컴포넌트 분리

---
+++ src/app/main/_components/asmr/ASMR.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import React from 'react';
+import styles from '../../../../styles/main/asmr.module.css';
+import { ASMRProps, Asmr } from '../../_interface/ModalInterface';
+import Headers from '../Header';
+import ImageItem from '../ImageItem';
+import { useBackgroundData } from './useAsmrData';
+
+const ASMR: React.FC<ASMRProps> = ({ playAudio, stopAudio, currentAsmr, isModalOpen, closeModal }) => {
+
+    const { asmrAudios, userAsmrAudios } = useBackgroundData();
+
+    const handleAudioPlay = (asmr: Asmr) => {
+        // 현재 재생 중인 ASMR과 클릭한 ASMR을 비교
+        if (currentAsmr && currentAsmr.id === asmr.id) {
+            stopAudio(); // 동일한 ASMR이면 정지
+        } else {
+            playAudio(asmr.url, asmr); // 다른 ASMR이면 재생
+        }
+    };
+
+    if (!isModalOpen) return null;
+
+    return (
+        <>
+            <div
+                className={styles.main_div}
+                onClick={(e: any) => e.stopPropagation()}
+            >
+                <Headers title="ASMR 선택" onClick={closeModal} />
+                <div className={styles.out_div}>
+                    <div className={styles.img_div}>
+                        {
+                            asmrAudios.map((asmr: Asmr) => {
+                                const isLocked = !userAsmrAudios.some(userAsmr => userAsmr.id === asmr.id);
+                                const isActive = currentAsmr && currentAsmr.id === asmr.id;
+                                return (
+                                    <ImageItem
+                                        key={asmr.id}
+                                        id={asmr.id}
+                                        fileName={asmr.fileName}
+                                        imageUrl={asmr.url}
+                                        isLocked={isLocked}
+                                        onSave={() => handleAudioPlay(asmr)}
+                                        isActive={isActive}
+                                        lockIcon={<span>🔒</span>}
+                                        actionIcon={isActive ? <span>▶</span> : <span>⏸</span>}
+                                    />
+                                )
+                            })}
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default ASMR;
