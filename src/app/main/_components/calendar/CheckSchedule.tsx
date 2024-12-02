b60e4a5f12f10e10a3eaf19b0d9ebe848127ef5b
jwj9127 <jwj9127@naver.com>
2024-12-02 17:00:38 +0900

    [feat]: 캘린더 일정확인 컴포넌트 추가

---
+++ src/app/main/_components/calendar/CheckSchedule.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import style from '../../../../../styles/main/calendar.module.css';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../../../../../store/store';
+import { deleteCalendarAPI, getCalendarAPI } from '../../../../../store/main/calendarAPI';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { Schedule } from '../../_interface/ModalInterface';
+import Swal from 'sweetalert2';
+
+export default function CheckSchedule() {
+
+    const dispatch = useDispatch<AppDispatch>();
+    const userId = window.localStorage.getItem('userId');
+    const [myDate, setMyDate] = useState([]);
+
+    dispatch(getCalendarAPI(userId!))
+        .unwrap()
+        .then((response) => {
+            setMyDate(response.data)
+        })
+        .catch(console.error);
+
+    const formatDateToString = (dateArray: any) => {
+        if (!dateArray || dateArray.length < 3) return '';
+        const [year, month, day] = dateArray;
+        return `${year}.${String(month).padStart(2, '0')}.${String(day).padStart(2, '0')}`;
+    };
+
+    const deleteCalendar = (id: any) => {
+        dispatch(deleteCalendarAPI(id))
+            .unwrap()
+            .then((response) => {
+                if (response.status == 200) {
+                    Swal.fire({
+                        title: "일정을 삭제했습니다"
+                    });
+                }
+            })
+            .catch(error => {
+                if (error.response && error.response.status === 404) {
+                    Swal.fire({
+                        title: "일정이 삭제 되지 않았습니다."
+                    })
+                }
+            });
+    }
+
+    return (
+        <>
+            <div className={style.check_schedule_div}>
+                <p className={style.check_schedule_top}>일정</p>
+                <div className={style.check_schedule_main_div}>
+                    {myDate.map((schedule: Schedule) => (
+                        <div key={schedule.id} className={style.check_schedule_content_div}>
+                            <div className={style.check_schedule_top_div}>
+                                <div className={style.check_schedule_date_div}>
+                                    <p>{formatDateToString(schedule.startDay)} ~ {formatDateToString(schedule.endDay)}</p>
+                                </div>
+                                <FontAwesomeIcon icon={faTrashCan} size='2x' onClick={() => deleteCalendar(schedule.id)} className={style.check_schedule_close} />
+                            </div>
+                            <p className={style.check_schedule_content}>{schedule.title}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+
+}
