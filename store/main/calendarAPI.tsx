1e0c1305a8c163d17885f35c07f87619ca5bfb83
jwj9127 <jwj9127@naver.com>
2024-12-02 17:01:48 +0900

    [feat]: 캘린더 API 추가

---
+++ store/main/calendarAPI.tsx
@@ -0,0 +1,114 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+    asmrs: [],
+    asmr: null,
+    loading: false,
+    error: null as string | null | undefined,
+};
+
+const calendarAPI = createSlice({
+    name: 'calendarAPI',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        // makeCalendarAPI
+        builder
+            .addCase(makeCalendarAPI.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(makeCalendarAPI.fulfilled, (state, action) => {
+                state.asmrs = action.payload;
+                state.loading = false;
+            })
+            .addCase(makeCalendarAPI.rejected, (state, action) => {
+                state.error = action.error.message;
+                state.loading = false;
+            });
+
+        // getCalendarAPI
+        builder
+            .addCase(getCalendarAPI.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(getCalendarAPI.fulfilled, (state, action) => {
+                state.asmrs = action.payload;
+                state.loading = false;
+            })
+            .addCase(getCalendarAPI.rejected, (state, action) => {
+                state.error = action.error.message;
+                state.loading = false;
+            });
+
+        // putCalendarAPI
+        builder
+            .addCase(putCalendarAPI.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(putCalendarAPI.fulfilled, (state, action) => {
+                state.asmr = action.payload;
+                state.loading = false;
+            })
+            .addCase(putCalendarAPI.rejected, (state, action) => {
+                state.error = action.error.message;
+                state.loading = false;
+            });
+
+        // deleteCalendarAPI
+        builder
+            .addCase(deleteCalendarAPI.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(deleteCalendarAPI.fulfilled, (state, action) => {
+                state.asmr = action.payload;
+                state.loading = false;
+            })
+            .addCase(deleteCalendarAPI.rejected, (state, action) => {
+                state.error = action.error.message;
+                state.loading = false;
+            });
+    }
+});
+
+export const makeCalendarAPI = createAsyncThunk(
+    '/calendar',
+    async (calendarDTO: { userId: string; title: string; startDay: Date; endDay: Date; }) => {
+        const response = await axios.post('/calendar', calendarDTO);
+        return response.data;
+    }
+);
+
+export const getCalendarAPI = createAsyncThunk(
+    '/calendar/{userId}',
+    async (userId: string) => {
+        const response = await axios.get(`/calendar/${userId}`);
+        return response.data;
+    }
+);
+
+export const putCalendarAPI = createAsyncThunk(
+    '/calendar/{calendar_id}',
+    async ({ calendar_id, calendarDTO }: { calendar_id: string; calendarDTO: { title: string; content: string; startDay: Date; endDay: Date; } }) => {
+        const response = await axios.put(`/calendar/${calendar_id}`, calendarDTO, {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        return response.data;
+    }
+);
+
+export const deleteCalendarAPI = createAsyncThunk(
+    '/calendar/{calendar_id}',
+    async (calendar_id: string) => {
+        const response = await axios.get(`/calendar/${calendar_id}`);
+        return response.data;
+    }
+);
+
+export default calendarAPI.reducer;
