001af24a95c108bfe7f6a1e7aa3585504b641c40
jwj9127 <jwj9127@naver.com>
2024-12-06 03:33:04 +0900

    [feat]: TodoList 템플릿 컴포넌트 추가

---
+++ src/app/main/_components/todolist/TodoTemplate.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { TodoTemplateProps } from '../../_interface/ModalInterface';
+
+function TodoTemplate({ children, closeModal }: TodoTemplateProps) {
+    return (
+        <div className="main_page_todolist_template">
+            <div className="main_page_todolist_template_title">
+                <p>Todo List</p>
+                <FontAwesomeIcon icon={faX} className='main_page_todolist_template_title_out' onClick={closeModal}/>
+            </div>
+            <div className="main_page_todolist_template_content">{children}</div>
+        </div>
+    )
+}
+
+export default TodoTemplate;
