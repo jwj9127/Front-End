daec491c3604217606499727128a68b299b5c677
jwj9127 <jwj9127@naver.com>
2024-12-06 03:36:22 +0900

    [feat]: TodoList 편집 컴포넌트 추가

---
+++ src/app/main/_components/todolist/Edit.tsx
@@ -0,0 +1,49 @@
+import { useCallback, useEffect, useState } from 'react';
+import { Todo } from '../../_interface/ModalInterface';
+
+interface ToDoEditProps {
+    selectedTodo: Todo | null;
+    onUpdate: (todoDTO: { id: string; title: string }) => void;
+}
+
+const ToDoEdit: React.FC<ToDoEditProps> = ({ selectedTodo, onUpdate }) => {
+    const [value, setValue] = useState('');
+    const todoDTO = { id: selectedTodo!.id, title: value };
+
+    const onChange = useCallback((e: any) => {
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(
+        (e: any) => {
+            if (selectedTodo) {
+                onUpdate(todoDTO);
+                setValue('');
+            }
+            e.preventDefault();
+        },
+        [onUpdate, value, selectedTodo],
+    );
+
+    useEffect(() => {
+        if (selectedTodo) {
+            setValue(selectedTodo.title);
+        }
+    }, [selectedTodo]);
+
+    return (
+        <div className="background">
+            <form onSubmit={onSubmit} className="todoedit__insert">
+                <h2>수정하기</h2>
+                <input
+                    onChange={onChange}
+                    value={value}
+                    placeholder="할 일을 입력하세요"
+                />
+                <button type="submit">수정하기</button>
+            </form>
+        </div>
+    );
+};
+
+export default ToDoEdit;
