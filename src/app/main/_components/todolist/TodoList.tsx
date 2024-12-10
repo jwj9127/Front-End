ce85b2ea779875bd2cafd3db96803bf6575b048a
jwj9127 <jwj9127@naver.com>
2024-12-10 18:27:24 +0900

    [docs]: 콘솔 찍어보기

--- src/app/main/_components/todolist/TodoList.tsx
+++ src/app/main/_components/todolist/TodoList.tsx
@@ -21,6 +21,8 @@ const TodoList: React.FC<ModalProps> = ({ isModalOpen, closeModal }) => {
     const [insertToggle, setInsertToggle] = useState(false);
 
     useEffect(() => {
+        console.log('왜 안되?')
+        console.log(isModalOpen)
         if (isModalOpen === true) {
             dispatch(getTodoAPI(userId!))
                 .unwrap()
