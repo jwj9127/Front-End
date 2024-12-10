9be92b5c6fe4abccd23a3ee46515c36e64d37194
jwj9127 <jwj9127@naver.com>
2024-12-10 20:59:36 +0900

    [dosc]:  헤더 div 추가

--- src/app/main/_components/calendar/CheckSchedule.tsx
+++ src/app/main/_components/calendar/CheckSchedule.tsx
@@ -1,61 +1,63 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import style from '../../../../../styles/main/calendar.module.css';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../../../../../store/store';
 import { deleteCalendarAPI, getCalendarAPI } from '../../../../../store/main/calendarAPI';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { faTrashCan, faX } from '@fortawesome/free-solid-svg-icons';
 import { Schedule } from '../../_interface/ModalInterface';
 import Swal from 'sweetalert2';
 
 interface CheckScheduleProps {
-    isViewingSchedule: boolean;
+    setIsViewingSchedule: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const CheckSchedule: React.FC<CheckScheduleProps> = ({ isViewingSchedule }) => {
+const CheckSchedule: React.FC<CheckScheduleProps> = ({ setIsViewingSchedule }) => {
 
     const dispatch = useDispatch<AppDispatch>();
     const userId = window.localStorage.getItem('userId');
     const [myDate, setMyDate] = useState([]);
 
-    if (isViewingSchedule === true) {
+    useEffect(() => {
         dispatch(getCalendarAPI(userId!))
             .unwrap()
             .then((response) => {
-                setMyDate(response.data)
+                setMyDate(response);
             })
             .catch(console.error);
-    }
+    }, [setIsViewingSchedule])
 
-    const formatDateToString = (dateArray: any) => {
-        if (!dateArray || dateArray.length < 3) return '';
-        const [year, month, day] = dateArray;
-        return `${year}.${String(month).padStart(2, '0')}.${String(day).padStart(2, '0')}`;
+    const formatDateToString = (dateString: string) => {
+        const date = new Date(dateString);
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${month}-${day}`;
     };
+
 
     const deleteCalendar = (id: any) => {
         dispatch(deleteCalendarAPI(id))
             .unwrap()
-            .then((response) => {
-                if (response.status == 200) {
-                    Swal.fire({
-                        title: "일정을 삭제했습니다"
-                    });
-                }
+            .then(() => {
+                Swal.fire({
+                    title: "일정을 삭제했습니다"
+                });
+                setIsViewingSchedule(false);
             })
-            .catch(error => {
-                if (error.response && error.response.status === 404) {
-                    Swal.fire({
-                        title: "일정이 삭제 되지 않았습니다."
-                    })
-                }
+            .catch(() => {
+                Swal.fire({
+                    title: "일정이 삭제 되지 않았습니다."
+                })
             });
     }
 
     return (
         <>
             <div className={style.check_schedule_div} onClick={(e) => e.stopPropagation()}>
-                <p className={style.check_schedule_top}>일정</p>
+                <div className={style.check_schedule_header}>
+                    <p className={style.check_schedule_top}>일정</p>
+                    <FontAwesomeIcon icon={faX} color='white' className={style.check_schedule_close} onClick={() => setIsViewingSchedule(false)} />
+                </div>
                 <div className={style.check_schedule_main_div}>
                     {myDate && myDate.length > 0 ? (
                         myDate.map((schedule: Schedule) => (
@@ -64,7 +66,7 @@ const CheckSchedule: React.FC<CheckScheduleProps> = ({ isViewingSchedule }) => {
                                     <div className={style.check_schedule_date_div}>
                                         <p>{formatDateToString(schedule.startDay)} ~ {formatDateToString(schedule.endDay)}</p>
                                     </div>
-                                    <FontAwesomeIcon icon={faTrashCan} size='2x' onClick={() => deleteCalendar(schedule.id)} className={style.check_schedule_close} />
+                                    <FontAwesomeIcon icon={faTrashCan} size='2x' onClick={() => deleteCalendar(schedule.id)} className={style.schedule_delete} />
                                 </div>
                                 <p className={style.check_schedule_content}>{schedule.title}</p>
                             </div>
