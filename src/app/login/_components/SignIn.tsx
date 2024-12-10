b553b75a0de96adc9b726a4a505ef78899bfaeed
jwj9127 <jwj9127@naver.com>
2024-12-10 17:31:55 +0900

    [feat]: 토큰 추가

--- src/app/login/_components/SignIn.tsx
+++ src/app/login/_components/SignIn.tsx
@@ -31,6 +31,7 @@ export default function SignIn() {
                     title: "로그인 성공"
                 });
                 window.localStorage.setItem('userId', response.userId);
+                window.localStorage.setItem('token', response.headers.authorization);
                 router.push('/main');
             })
             .catch((error) => {
