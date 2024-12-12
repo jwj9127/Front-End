d22faed2682de12c8a0e9044f1e29638a5c28ef1
jwj9127 <jwj9127@naver.com>
2024-12-13 03:10:19 +0900

    [feat]: 추천 영상 리스트 컴포넌트 추가

---
+++ src/app/music/_components/CategoryList.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from "../../../../store/store";
+import { ListProp } from "../_interface/MusicInterface";
+import styles from '../../../../styles/music/music.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { addListByUrlAPI, getUserListAPI } from "../../../../store/music/musicAPI";
+import Swal from "sweetalert2";
+
+export default function CategoryList() {
+
+    const dispatch = useDispatch<AppDispatch>();
+    const userId = window.localStorage.getItem('userId');
+    const list = useSelector((state: RootState) => state.musicAPI.categoryList);
+
+    const addUrlList = (videoUrl: any) => {
+        const addUrlDTO = {
+            userId: userId!,
+            videoUrl: videoUrl!
+        }
+        dispatch(addListByUrlAPI(addUrlDTO))
+            .unwrap()
+            .then(() => {
+                Swal.fire({
+                    title: "추가 완료"
+                })
+                dispatch(getUserListAPI(userId!));
+            })
+            .catch((error) => {
+                if (error && error.status === 400) {
+                    Swal.fire({
+                        text: error.message
+                    })
+                }
+            });
+    }
+
+    return (
+        <>
+            <div className={styles.list_div}>
+                {list.length > 0 ?
+                    (list).map((prop: ListProp) => (
+                        <div className={styles.prop_div}>
+                            <div
+                                className={styles.list_url}
+                                style={{ backgroundImage: `url(${prop.thumbnailUrl})` }}
+                            ></div>
+                            <div className={styles.title_div}>
+                                <p>{prop.title}</p>
+                                <p>{prop.channelTitle}</p>
+                            </div>
+                            <FontAwesomeIcon
+                                icon={faPlus}
+                                size="2x"
+                                className={styles.plus}
+                                onClick={() => addUrlList(prop.videoUrl)}
+                            />
+                        </div>
+                    )) :
+                    (
+                        <div className={styles.list_div}>
+                            <p>영상 정보가 없어요!</p>
+                        </div>
+                    )
+                }
+            </div >
+        </>
+    )
+}
