9c1b9052dee3e9998ceb9d635d41f594dfbadc49
jwj9127 <jwj9127@naver.com>
2024-12-12 12:31:43 +0900

    [docs]: 라우터 제거

--- src/app/login/_components/SignUp.tsx
+++ src/app/login/_components/SignUp.tsx
@@ -74,8 +74,6 @@ export default function SignUp() {
                     Swal.fire({
                         title: response
                     });
-                    router.push('/login');
-                    console.log(response);
                 })
                 .catch((error) => {
                     Swal.fire({
