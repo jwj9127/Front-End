95559941c166de31f58f569165a52bd7c47973d8
jwj9127 <jwj9127@naver.com>
2024-12-14 17:37:53 +0900

    [test]: 콘솔

--- util/response.tsx
+++ util/response.tsx
@@ -2,7 +2,6 @@ import Swal from "sweetalert2";
 
 export const response = (response: any) => {
     if (response && response.status === 200) {
-        console.log(response);
         return Swal.fire({
             text: response.message
         });
